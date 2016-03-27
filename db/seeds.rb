6d7059c3097e08e3b199f7dcbe17912743567070
Braeden Mulligan <braeden.mulligan@gmail.com>
2016-03-27 16:30:29 -0700

    Create admins and an index-users page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(name: "administrator", email: "administrator@email.com",
+	password: "adminpassword", password_confirmation "adminpassword",
+	admin: true)
