05bcbc6a4e92874fc9750307b852a08b7b032105
jasoncoo1013 <jcoo@sfu.ca>
2016-03-28 00:15:53 +0000

    restored about page

--- db/schema.rb
+++ db/schema.rb
@@ -30,10 +30,10 @@ ActiveRecord::Schema.define(version: 20160327222846) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "remember_digest"
-    t.boolean  "admin"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
