75b0ba9cbebc71982d32e8dd03de53bd262c022d
jasoncoo1013 <jcoo@sfu.ca>
2016-04-03 00:38:23 +0000

    completed search bar

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -3,13 +3,16 @@ class MapsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:update]
 
   def course
+    
+    @courses = Course.all
     @coords = []
     if params[:id]
       @c = Course.find(params[:id])
       @coords = @c.coordinates
     end
   	
-    render :layout => false
+  render :layout => false
+#    render :searchItems
   end
 
 end
