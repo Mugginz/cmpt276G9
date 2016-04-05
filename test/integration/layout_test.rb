de188b85ca834b3e25b2f95da0776675d4c1d79a
Braeden Mulligan <braeden.mulligan@gmail.com>
2016-04-04 20:20:17 -0700

    Test homepage

--- test/integration/layout_test.rb
+++ test/integration/layout_test.rb
@@ -5,12 +5,26 @@ class LayoutTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
-  test "layout links" do
+  test "static elements" do
   	get root_path
   	assert_template 'statics/home'
   	assert_select "a[href=?]", root_path
   	assert_select "a[href=?]", about_path
   	assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", news_path
+  end
+
+  test "login lements" do
+    get root_path
+    assert_select "input[value=?]", "Login"
+    assert_select "a[href=?]", signup_url
+    assert_select "input[type=?]", "checkbox"
+  end
+
+  test "game elements" do
+    get root_path
+    assert_select "a[href=?]", map_path
+    assert_select "a[href=?]", courses_path
   end
 
 end
