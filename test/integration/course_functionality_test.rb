7b73bcb1715170540c77a580895e110a4125330c
Braeden Mulligan <braeden.mulligan@gmail.com>
2016-04-04 22:06:53 -0700

    Test course model

--- test/integration/course_functionality_test.rb
+++ test/integration/course_functionality_test.rb
@@ -7,25 +7,26 @@ class CourseFunctionalityTest < ActionDispatch::IntegrationTest
 
   def setup
     @c = courses(:one)
+    @cgood = {name: "sample", region: "", coords: "[[2,3],[4,5]]"}
+    @cbad = {name: "", region: "", coordinates: []}
   end
 
   test "invalid creation" do
     get create_path
     pre = Course.count
-    post courses_path, course: { name:  "", region: "", coordinates: [] }
+    post create_path, @cbad
     aft = Course.count
+
 	assert_equal(pre, aft)
-
-    assert_template 'courses/index'
   end
-
 
   test "valid creation" do
   	get create_path
   	pre = Course.count
-	post courses_path, @c
+  	post create_path, @cgood
     aft = Course.count
+
     assert_not_equal(pre, aft)
   end
- 
+
 end
